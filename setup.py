37fd2483e404bad1773d6a84e416187205cd8d38
rwBrightly <roger@brightly.com>
2025-01-23 17:29:11 -0500

    add setup for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ach",
+    version="0.3",
+    packages=["ach"],
+    url="https://github.com/rogerBrightly/nb-ach",
+    license="MIT License",
+    description="Library to create and parse ACH files (NACHA) compatible with WEB Premium Entry (PPD) and CCD.",
+)
